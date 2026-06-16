fff15db214393ad837aec4503be554d486a51034
mujebpc-hub <mujebpc@gmail.com>
2026-06-16 13:13:46 +0530

    Create game.js

---
+++ game.js
@@ -0,0 +1,102 @@
+const playBtn = document.getElementById("playBtn");
+const homeScreen = document.getElementById("homeScreen");
+const gameScreen = document.getElementById("gameScreen");
+
+const canvas = document.getElementById("mazeCanvas");
+const ctx = canvas.getContext("2d");
+
+let level = 1;
+let coins = 0;
+
+let maze = [
+    [0,0,0,1,0],
+    [1,1,0,1,0],
+    [0,0,0,0,0],
+    [0,1,1,1,0],
+    [0,0,0,1,0]
+];
+
+let player = {x:0, y:0};
+let goal = {x:4, y:4};
+
+const cellSize = 80;
+
+playBtn.onclick = () => {
+    homeScreen.classList.add("hidden");
+    gameScreen.classList.remove("hidden");
+    drawMaze();
+};
+
+function drawMaze(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    for(let y=0; y<maze.length; y++){
+        for(let x=0; x<maze[y].length; x++){
+            if(maze[y][x] === 1){
+                ctx.fillStyle = "black";
+                ctx.fillRect(x*cellSize,y*cellSize,cellSize,cellSize);
+            }else{
+                ctx.strokeRect(x*cellSize,y*cellSize,cellSize,cellSize);
+            }
+        }
+    }
+
+    // Goal
+    ctx.fillStyle = "green";
+    ctx.fillRect(goal.x*cellSize+20, goal.y*cellSize+20, 40, 40);
+
+    // Player
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.arc(
+        player.x*cellSize+40,
+        player.y*cellSize+40,
+        20,
+        0,
+        Math.PI*2
+    );
+    ctx.fill();
+
+    updateUI();
+}
+
+function movePlayer(direction){
+    let newX = player.x;
+    let newY = player.y;
+
+    if(direction === "up") newY--;
+    if(direction === "down") newY++;
+    if(direction === "left") newX--;
+    if(direction === "right") newX++;
+
+    if(
+        newX >= 0 &&
+        newY >= 0 &&
+        newX < maze[0].length &&
+        newY < maze.length &&
+        maze[newY][newX] === 0
+    ){
+        player.x = newX;
+        player.y = newY;
+    }
+
+    if(player.x === goal.x && player.y === goal.y){
+        coins += 10;
+        level++;
+
+        alert("Level Complete! +10 Coins");
+
+        player = {x:0, y:0};
+
+        updateUI();
+    }
+
+    drawMaze();
+}
+
+function updateUI(){
+    document.getElementById("levelDisplay").innerText = level;
+    document.getElementById("coinDisplay").innerText = coins;
+    document.getElementById("gameLevel").innerText = level;
+    document.getElementById("gameCoins").innerText = coins;
+}
